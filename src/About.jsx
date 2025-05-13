7cf3c8ac9b1bfe5776f585e275a7c4067b9f5617
obaro <dietakeobaro7@gmail.com>
2025-05-13 23:17:04 +0100

    Initial Commit

---
+++ src/About.jsx
@@ -0,0 +1,13 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio fugiat
+        inventore dolorem vel porro expedita deserunt magni ab vero possimus.
+      </p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/Home.jsx
@@ -0,0 +1,13 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio fugiat
+        inventore dolorem vel porro expedita deserunt magni ab vero possimus.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
